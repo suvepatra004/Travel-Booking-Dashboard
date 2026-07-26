09a619527fc7de22ba9458e0a4df44e314272d52
Suvendu Kumar Patra <suvepatra2003@gmail.com>
2026-07-27 01:12:28 +0530

    Add sidebar navigation and user footer to NavItems component; update dependencies

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,13 +1,54 @@
 import React from "react";
-import { Link } from "react-router";
+import { Link, NavLink } from "react-router";
+import { sidebarItems } from "~/constants";
+import { cn } from "../lib/utils";
+import { isatty } from "tty";
 
 const NavItems = () => {
+  const user = {
+    name: "Suvendu",
+    email: "suve@mail.com",
+    imageUrl: "/public/assets/images/david.webp",
+  };
   return (
     <section className="nav-items">
       <Link to="/" className="link-logo">
         <img src="/assets/icons/logo.svg" alt="logo" className="size:30px" />
         <h2>TravelGo</h2>
       </Link>
+      <div className="container">
+        <nav>
+          {sidebarItems.map(({ id, href, icon, label }) => (
+            <NavLink to={href} key={id}>
+              {({ isActive }: { isActive: boolean }) => (
+                <div
+                  className={cn("group nav-item", {
+                    "bg-primary-100 text-white!": isActive,
+                  })}
+                >
+                  <img
+                    src={icon}
+                    alt={label}
+                    className={`group-hover: brightness-0 size-0 group-hover:invert ${isActive ? "brightness-0 invert" : "text-dark-200"}`}
+                  />
+                  {label}
+                </div>
+              )}
+            </NavLink>
+          ))}
+        </nav>
+        <footer className="nav-footer">
+          <img
+            src={user?.imageUrl || "/public/assets/images/david.webp"}
+            alt={user?.name || "David"}
+          />
+          <article>
+            <h2>{user?.name}</h2>
+            <h2>{user?.email}</h2>
+          </article>
+          <button></button>
+        </footer>
+      </div>
     </section>
   );
 };
